1b11012e55d36b4e629594697a32251e597e5236
TakimiShinoda <godgodx4@gmail.com>
2019-07-04 18:56:31 +0900

    display start text

--- src/scenes/preload.ts
+++ src/scenes/preload.ts
@@ -1,12 +1,26 @@
 import * as Phaser from "phaser";
 
 export class Preload extends Phaser.Scene {
+  private startText?: Phaser.GameObjects.Text
+
+  private bk_color: string = '0xe08734'
+  private fontStyle: Phaser.Types.GameObjects.Text.TextSyle = {
+    color: 'red',
+    fontSize: '70px'
+  }
+
   init() {
     console.log("Preloading");
   }
 
   preload () {
-    console.log("Load things necessary for Game scene");
-    this.scene.start("game");
+    console.log("Load things necessary for Game scene")
+  }
+
+  create() {
+    this.cameras.main.setBackgroundColor(this.bk_color)
+    this.startText = this.add.text(400, 300, 'START', this.fontStyle)
+
+    this.startText.setOrigin(0.5)
   }
 }
