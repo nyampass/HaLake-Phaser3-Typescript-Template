f4aefd79be569f39e7002c10be6a7d596db5fe8d
TakimiShinoda <godgodx4@gmail.com>
2019-07-06 13:41:58 +0900

    apply map for scene game

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,16 +1,44 @@
 import * as Phaser from "phaser";
 
 export class Game extends Phaser.Scene {
+  private map?: Phaser.Tilemaps.Tilemap
+  private tiles?: Phaser.Tilemaps.Tileset
+  private map_ground_layer?: Phaser.Tilemaps.StaticTilemapLayer
+
+  private map_ground: number[][] = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+  ] // 20 * 15
+
   init() {
     console.log('Initializing.')
   }
 
   preload() {
     console.log('Load assets.')
+
+    this.load.image('mapTiles', `../../assets/images/map_tile.png`)
   }
 
   create() {
     console.log('Draw objects to canvas.')
+
+    this.map = this.make.tilemap({ data: this.map_ground, tileWidth: 40, tileHeight: 40 })
+    this.tiles = this.map.addTilesetImage(`mapTiles`)
+    this.map_ground_layer = this.map.createStaticLayer(0, this.tiles, 0, 0)
   }
 
   update() {
