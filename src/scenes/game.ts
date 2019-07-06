5a0301694135c6a221c340c71f1eb181915469df
TakimiShinoda <godgodx4@gmail.com>
2019-07-06 17:58:00 +0900

    add animation for hero

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -6,6 +6,13 @@ export class Game extends Phaser.Scene {
   private map_ground_layer?: Phaser.Tilemaps.StaticTilemapLayer
   private hero?: Phaser.GameObjects.Sprite
 
+  private heroAnims: {key: string, frameStart: number, frameEnd: number}[] = [
+    {key: 'walk_front', frameStart: 0, frameEnd: 2},
+    {key: 'walk_back', frameStart: 9, frameEnd: 11},
+    {key: 'walk_left', frameStart: 3, frameEnd: 5},
+    {key: 'walk_right',frameStart: 6, frameEnd: 8},
+  ]
+
   private map_ground: number[][] = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -43,9 +50,32 @@ export class Game extends Phaser.Scene {
     this.map_ground_layer = this.map.createStaticLayer(0, this.tiles, 0, 0)
 
     this.hero = this.add.sprite(400, 300, 'hero', 0)
+
+    for(let heroAnim of this.heroAnims){
+      if(this.anims.create(this.heroAnimConfig(heroAnim)) === false) continue
+        
+      this.hero.anims.load(heroAnim.key)
+    }
+
+    this.hero.anims.play('walk_front')
   }
 
   update() {
     console.log('Call at every frames.')
+  }
+
+  private heroAnimConfig(config: {key: string, frameStart: number, frameEnd: number}): Phaser.Types.Animations.Animation{
+    return {
+      key: config.key,
+      frames: this.anims.generateFrameNumbers(
+        `hero`, 
+        {
+          start: config.frameStart,
+          end: config.frameEnd
+        }
+      ),
+      frameRate: 8,
+      repeat: -1
+    }
   }
 }
