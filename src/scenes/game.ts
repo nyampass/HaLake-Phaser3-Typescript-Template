2e1020668fd5c08d5808b93253edb557a1a82392
TakimiShinoda <godgodx4@gmail.com>
2019-07-06 15:43:49 +0900

    add hero

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -4,6 +4,7 @@ export class Game extends Phaser.Scene {
   private map?: Phaser.Tilemaps.Tilemap
   private tiles?: Phaser.Tilemaps.Tileset
   private map_ground_layer?: Phaser.Tilemaps.StaticTilemapLayer
+  private hero?: Phaser.GameObjects.Sprite
 
   private map_ground: number[][] = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -31,6 +32,7 @@ export class Game extends Phaser.Scene {
     console.log('Load assets.')
 
     this.load.image('mapTiles', `../../assets/images/map_tile.png`)
+    this.load.spritesheet('hero', '../../assets/images/hero.png', { frameWidth: 32, frameHeight: 32 });
   }
 
   create() {
@@ -39,6 +41,8 @@ export class Game extends Phaser.Scene {
     this.map = this.make.tilemap({ data: this.map_ground, tileWidth: 40, tileHeight: 40 })
     this.tiles = this.map.addTilesetImage(`mapTiles`)
     this.map_ground_layer = this.map.createStaticLayer(0, this.tiles, 0, 0)
+
+    this.hero = this.add.sprite(400, 300, 'hero', 0)
   }
 
   update() {
